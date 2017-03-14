ab52035a3839486b37d50d93044625591ef2f3c4
irneb <irne.barnard@yahoo.com>
2017-03-14 12:26:32 +0200

    Array tests
    Added extra tests on a raw array

--- SearchBenchmark/CollectionTests.cs
+++ SearchBenchmark/CollectionTests.cs
@@ -5,8 +5,18 @@ using BenchmarkDotNet.Attributes.Exporters;
 
 namespace SearchBenchmark
 {
+    [AsciiDocExporter]
+    [CsvExporter]
+    [CsvMeasurementsExporter]
+    [HtmlExporter]
+    [MarkdownExporter]
+    [PlainExporter]
     public class CollectionTests
     {
+        public int LastValue { get { return array[Length - 1]; } }
+
+        int[] array = new int[1];
+
         List<int> list = new List<int>();
 
         LinkedList<int> lList = new LinkedList<int>();
@@ -20,16 +30,18 @@ namespace SearchBenchmark
         {
             get
             {
-                return list.Count;
+                return array.Length;
             }
             set
             {
+                array = new int[value];
                 list.Clear();
                 lList.Clear();
                 dict.Clear();
                 sDict.Clear();
                 for (int i = 0; i < value; ++i)
                 {
+                    array[i] = i;
                     list.Add(i);
                     lList.AddLast(i);
                     dict[i] = i;
@@ -39,16 +51,30 @@ namespace SearchBenchmark
         }
 
         [Benchmark]
+        public int ArrayFind()
+        {
+            var val = LastValue;
+            return Array.IndexOf<int>(array, val);
+        }
+
+        [Benchmark]
+        public int ArrayBinarySearch()
+        {
+            var val = LastValue;
+            return Array.BinarySearch<int>(array, val);
+        }
+
+        [Benchmark]
         public int ListFind()
         {
-            var val = list[list.Count - 1];
+            var val = LastValue;
             return list.FindIndex(item => item.Equals(val));
         }
 
         [Benchmark]
         public int ListForEachSearch()
         {
-            var val = list[list.Count - 1];
+            var val = LastValue;
             var idx = 0;
             foreach (var item in list)
             {
@@ -64,7 +90,7 @@ namespace SearchBenchmark
         [Benchmark]
         public int ListForSearch()
         {
-            var val = list[list.Count - 1];
+            var val = LastValue;
             for (int i = 0; i < Length; ++i)
             {
                 if (val.Equals(list[i]))
@@ -78,36 +104,35 @@ namespace SearchBenchmark
         [Benchmark]
         public int ListBinarySearch()
         {
-            var val = list[list.Count - 1];
-            //seq.Next;
+            var val = LastValue;
             return list.BinarySearch(val);
         }
 
         [Benchmark]
         public LinkedListNode<int> LinkedListFind()
         {
-            var val = lList.Last.Value;
+            var val = LastValue;
             return lList.Find(val);
         }
 
         [Benchmark]
         public bool DictionaryContainsKey()
         {
-            var val = list[Length - 1];
+            var val = LastValue;
             return dict.ContainsKey(val);
         }
 
         [Benchmark]
         public bool DictionaryContainsValue()
         {
-            var val = list[Length - 1];
+            var val = LastValue;
             return dict.ContainsValue(val);
         }
 
         [Benchmark]
         public int DictionaryTryGetValue()
         {
-            var val = list[Length - 1];
+            var val = LastValue;
             int found;
             return dict.TryGetValue(val, out found) ? found : -1;
         }
@@ -115,21 +140,21 @@ namespace SearchBenchmark
         [Benchmark]
         public bool SortedDictionaryContainsKey()
         {
-            var val = list[Length - 1];
+            var val = LastValue;
             return sDict.ContainsKey(val);
         }
 
         [Benchmark]
         public bool SortedDictionaryContainsValue()
         {
-            var val = list[Length - 1];
+            var val = LastValue;
             return sDict.ContainsValue(val);
         }
 
         [Benchmark]
         public int SortedDictionaryTryGetValue()
         {
-            var val = list[Length - 1];
+            var val = LastValue;
             int found;
             return sDict.TryGetValue(val, out found) ? found : -1;
         }
